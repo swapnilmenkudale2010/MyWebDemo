c2363f2c189087bbfa99dae2f9916dffcefaee6f
swapnilmenkudale2010 <46816911+swapnilmenkudale2010@users.noreply.github.com>
2019-02-07 18:59:24 +0530

    Add files via upload

---
+++ src/Systems.SteelToePOC.Web.Api/Controllers/ServiceMonitoring/NudgeController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Systems.SteelToePOC.Web.Api.Controllers
+{
+    using log4net;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class NudgeController : ControllerBase
+    {
+        private readonly ILog _logger;
+        public NudgeController(ILog log)
+        {
+            _logger = log;
+        }
+
+        [HttpGet, Route("")]
+        public ActionResult<bool> Get()
+        {
+            _logger.Info("NudgeController - Get");
+            return true;
+        }
+    }
+}
---
+++ src/Systems.SteelToePOC.Web.Api/Controllers/ServiceMonitoring/PingController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Systems.SteelToePOC.Web.Api.Controllers
+{
+    using log4net;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly ILog _logger;
+        public PingController(ILog log)
+        {
+            _logger = log;
+        }
+
+        [HttpGet, Route("")]
+        public ActionResult<bool>  Get()
+        {
+            _logger.Info("PingController - Get");
+            return true;
+        }
+    }
+}
