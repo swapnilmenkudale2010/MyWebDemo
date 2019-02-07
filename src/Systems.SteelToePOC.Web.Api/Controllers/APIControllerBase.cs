fb1b7b5ceb186f346eb104f852a8b63b41d52129
swapnilmenkudale2010 <46816911+swapnilmenkudale2010@users.noreply.github.com>
2019-02-07 18:57:06 +0530

    Add files via upload

---
+++ src/Systems.SteelToePOC.Web.Api/Controllers/APIControllerBase.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Systems.SteelToePOC.Web.Api.Controllers
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Formatting;
+    using System.Net.Http.Headers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+
+    public class APIControllerBase : ControllerBase
+    {
+        protected static HttpClient HttpClient { get; set; }
+
+        protected string WebApiPath { set; get; }
+
+        public APIControllerBase()
+        {
+            WebApiPath = GetConfigValue("AppSettings", nameof(WebApiPath));
+            HttpClient = new HttpClient();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="url"></param>
+        /// <param name="mediaTypeHeaderValue"></param>
+        /// <param name="method"></param>
+        /// <param name="content"></param>
+        /// <param name="formatter"></param>
+        /// <returns></returns>
+        protected static HttpRequestMessage CreateRequest<T>(Uri url, string mediaTypeHeaderValue, HttpMethod method, T content, MediaTypeFormatter formatter)
+        {
+            var request = new HttpRequestMessage { RequestUri = url };
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaTypeHeaderValue));
+            request.Method = method;
+            request.Content = new ObjectContent<T>(content, formatter);
+            return request;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="mediaTypeHeaderValue"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        protected static HttpRequestMessage CreateRequest(Uri url, string mediaTypeHeaderValue, HttpMethod method)
+        {
+            var request = new HttpRequestMessage { RequestUri = url };
+            if (!string.IsNullOrWhiteSpace(mediaTypeHeaderValue))
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaTypeHeaderValue));
+            }
+            request.Method = method;
+            return request;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpResponse"></param>
+        /// <returns></returns>
+        public static HttpResponseMessage CreateResponse(HttpResponseMessage httpResponse)
+        {
+            if (httpResponse != null)
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response.StatusCode = httpResponse.StatusCode;
+                response.Content = httpResponse.Content;
+                return response;
+            }
+            return httpResponse;
+        }
+
+        protected IActionResult CreateResponse(HttpStatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case HttpStatusCode.BadRequest: return BadRequest();
+                case HttpStatusCode.InternalServerError: return StatusCode(StatusCodes.Status500InternalServerError);
+                case HttpStatusCode.Unauthorized: return StatusCode(StatusCodes.Status401Unauthorized);
+                case HttpStatusCode.NotFound: return StatusCode(StatusCodes.Status404NotFound);
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        private static string GetConfigValue(string sectionName, string propertyName)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, false)
+                .Build();
+
+            return config?.GetSection(sectionName)?.GetSection(propertyName)?.Value ??
+              throw new InvalidOperationException($"{propertyName} could not be found in the config.");
+        }
+    }
+}
---
+++ src/Systems.SteelToePOC.Web.Api/Controllers/ManagementController.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Systems.SteelToePOC.Web.Api.Controllers
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json.Linq;
+
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class ManagementController : APIControllerBase
+    {
+        public ManagementController()
+        {
+        }
+
+        /// <summary>
+        /// Reports application health information
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("health")]
+        public async Task<IActionResult> Health()
+        {
+            string url = $"{WebApiPath}management/health";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports a set of trace information (such as the last 100 HTTP requests)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("trace")]
+        public async Task<IActionResult> Trace()
+        {
+            string url = $"{WebApiPath}management/trace";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Triggers the application configuration to be reloaded
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("refresh")]
+        public async Task<IActionResult> Refresh()
+        {
+            string url = $"{WebApiPath}management/refresh";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports the keys and values from the applications configuration
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("env")]
+        public async Task<IActionResult> Env()
+        {
+            string url = $"{WebApiPath}management/env";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports the http request timings for the application
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/requesttime")]
+        public async Task<IActionResult> RequestTime()
+        {
+            string url = $"{WebApiPath}management/metrics/http.server.request.time";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports the http request counts for the application
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/requestcount")]
+        public async Task<IActionResult> RequestCount()
+        {
+            string url = $"{WebApiPath}management/metrics/http.server.request.count";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports CLR threadpool metrics
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/clrthreadpoolactive")]
+        public async Task<IActionResult> ClrThreadpoolActive()
+        {
+            string url = $"{WebApiPath}management/metrics/clr.threadpool.active";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports CLR garbage collections
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/clrgccollections")]
+        public async Task<IActionResult> ClrGcCollections()
+        {
+            string url = $"{WebApiPath}management/metrics/clr.gc.collections";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Reports CLR threadpool availability
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/clrthreadpoolavail")]
+        public async Task<IActionResult> ClrThreadpoolAvail()
+        {
+            string url = $"{WebApiPath}management/metrics/clr.threadpool.avail";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+
+        /// <summary>
+        /// Reports CLR memory used
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("metrics/clrmemoryused")]
+        public async Task<IActionResult> ClrMemoryUsed()
+        {
+            string url = $"{WebApiPath}management/metrics/clr.memory.used";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Generates and reports a snapshot of the applications threads
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("dump")]
+        public async Task<IActionResult> Dump()
+        {
+            string url = $"{WebApiPath}management/dump";
+            return await SendRequest(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Generates and downloads a mini-dump of the application
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("heapdump")]
+        public async Task<IActionResult> HeapDump()
+        {
+            string url = $"{WebApiPath}management/heapdump";
+            using (var request = CreateRequest(new Uri(url), "application/octet-stream", HttpMethod.Get))
+            {
+                var response = await HttpClient.SendAsync(request).ConfigureAwait(false);
+
+                var stream = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+                var result = new FileContentResult(stream, "application/octet-stream")
+                {
+                    FileDownloadName = $"heap{DateTime.Now}.gz"
+                };
+                return result;
+            }
+        }
+
+        private async Task<IActionResult> SendRequest(string url)
+        {
+            using (var request = CreateRequest(new Uri(url), "application/json", HttpMethod.Get))
+            {
+                HttpResponseMessage response = await HttpClient.SendAsync(request).ConfigureAwait(false);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    JToken jsonFormatted = JValue.Parse(result);
+                    return Ok(jsonFormatted);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+            }
+        }
+    }
+}
